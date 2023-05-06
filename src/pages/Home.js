5e32e1e047f15562c7b12112a5a8f06b828ce4eb
tonyekim <otoobongekim@gmail.com>
2023-05-06 08:25:35 +0100

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,22 +5,22 @@ import { NavLink } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-       <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div class="container-fluid">
-      <a routerLink="/" class="navbar-brand" >Welcome</a>
-      
-      <div >
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-         
-          <li class="nav-item">
-            <NavLink to='/dashboard' class="nav-link active" >Navigate to dashboard!</NavLink>
-          </li>
-         
-        </ul>
-        
-      </div>
-    </div>
-  </nav>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+          <a routerLink="/" class="navbar-brand">Welcome to Hamoye React.js Project</a>
+
+          <div >
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+
+              <li class="nav-item">
+                {/* <NavLink to='/dashboard' class="nav-link active" >Navigate to dashboard!</NavLink> */}
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
       <div className='LoginPage'>
         <h1>Register & Login to Check Flight Information</h1>
       </div>
